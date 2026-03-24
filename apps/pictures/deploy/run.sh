0f4d0c29c6fec51c39b74099c347804f077eefc6
kalein.mondragon <kalein.mondragon@octopy.com>
2026-03-24 13:20:01 -0600

    REF: Move pictures app URL to `url.txt` and remove the `run.sh` deployment script and `type.txt` configuration.

--- apps/pictures/deploy/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-chromium-browser --kiosk --incognito --noerrdialogs --disable-infobars --check-for-update-interval=31536000 "https://fotografias-octytron.octopy.com/welcome"
