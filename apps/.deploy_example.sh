9da5fb72ca740204fd12479acda61de719c910eb
kalein.mondragon <kalein.mondragon@octopy.com>
2026-05-13 16:19:38 -0600

    Ignore apps folder and remove it from tracking

--- apps/.deploy_example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Created by Kaléin Tamaríz
-
-# The hub_backend.py dynamically assigns a port based on a hash of the folder name ("Slot Machine")
-PORT=$(python3 -c "import hashlib; print(3000 + (int(hashlib.md5(b'Slot Machine').hexdigest(), 16) % 5000))")
-
-echo "Starting Slot Machine web server on port $PORT"
-python3 -m http.server $PORT
